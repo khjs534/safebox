8ae3365921c3df910f33cc67b688c22ffbc25dcc
bradley simmons <bradleysimmons9@gmail.com>
2016-12-17 15:52:10 -0600

    refactor

--- run.py
+++ run.py
@@ -111,35 +111,6 @@ def incoming():
           message = "user not authorized"
     else:
       message = "user not authorized"
-       
-
-
-
-    # if incoming_message[0].lower() == 'set':
-    #   database = open('database.txt', 'r+')
-    #   tracking = database.readline().split(" ")
-    #   if not incoming_message[1] in tracking:
-    #     database.write(incoming_message[1] + " ")
-    #     message = "access code set to: " + incoming_message[1]
-    #   else:
-    #     message = "number already exists"
-    # elif incoming_message[0].lower() == 'unlock':
-    #   database = open('database.txt', 'r+')
-    #   data_array = database.readline().split(" ")
-    #   if incoming_message[1] in data_array:
-    #     message = "access granted"
-    #     database.close()
-    #     data_array.remove(incoming_message[1])
-    #     data_string = " ".join(data_array)
-    #     print data_string
-    #     database_2 = open('database.txt', 'w+')
-    #     database_2.write(data_string + " ")
-    #   else:
-    #     message = "access denied"
-    # else:
-    #   message = 'input not recognized'
-
-    print incoming_message
 
     resp = twilio.twiml.Response()
     resp.message(message)
