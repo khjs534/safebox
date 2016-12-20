3a748d39694ab99a9a8c5d78e2d19fa9dcdb06f2
Sean Kung <khjs534@gmail.com>
2016-12-20 14:42:13 -0600

    Implement take_picture in keypad.py

--- keypad.py
+++ keypad.py
@@ -3,12 +3,13 @@ import twilio.twiml
 import time
 import RPi.GPIO as GPIO
 from twilio.rest import TwilioRestClient
+from subprocess import call
 
 # add your twilio SID and token here
 ACCOUNT_SID = ""
 AUTH_TOKEN = ""
-
-client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+# uncomment line below when putting in your AUTH
+#client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
 
 def digit():
@@ -52,7 +53,8 @@ def message_admins(message):
       from_ = "",
       body = message
     )
-
+def take_picture():
+  call("fswebcam --no-banner pic.jpeg", shell=True)  
 
 while True:
   code = ""
@@ -63,9 +65,12 @@ while True:
   if code in data_array:
     unlock()
     message = "Safebox unlocked from keypad with code: " + code
-    message_admins(message)
+     # uncomment line below when not testing
+#    message_admins(message)
+    take_picture()
   elif code == "####":
     lock()
     message = "Safebox locked from keypad"
-    message_admins(message)
+    # uncomment line below when not testing
+#    message_admins(message)
 
